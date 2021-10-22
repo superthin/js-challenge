7e22248bbb4367121db75c647c4c20ab8c60738f
Super Thin <superthin@yahoo.com>
2021-10-22 09:11:07 +0700

    Loc LocNguyenXuan99
    https://daynhauhoc.com/u/LocNguyenXuan99

---
+++ js-codes/LocNguyenXuan99.js
@@ -0,0 +1,12 @@
+function myDateValid(dateStrVn) {
+    const date_regex = /^([0-9]{2})-([0-9]{2})-([0-9]{4})$/;
+    const date_match = input.match(date_regex)
+	if (!date_match){
+		console.log("Input's format is not vaild")
+		return false
+	}
+    let [d,m,y] = dateStrVn.split('-');
+    let myDate = new Date(+y, +m - 1, +d);
+    
+    return +d == myDate.getDay() && +m == myDate.getMonth() + 1 && +y ==  myDate.getYear() ;
+}
